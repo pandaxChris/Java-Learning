984f7bed2da6cf4bc4ad4a0f7dd35ccbc2d99f18
Chris Li <cli78822@gmail.com>
2016-10-08 23:41:00 -0400

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package chatserver;
 
 /**
  *
  * @author Chris Li
- * CS 3913 Java Homework 4
+ * 
  * Server code
  */
 
